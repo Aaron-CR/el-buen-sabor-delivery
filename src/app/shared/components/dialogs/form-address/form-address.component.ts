85888f84ee6ba519066df86d9eae5f959717ff0f
Aaron Castro <aaroncrcr@gmail.com>
2020-06-15 10:21:07 -0300

    form address

--- src/app/shared/components/dialogs/form-address/form-address.component.ts
+++ src/app/shared/components/dialogs/form-address/form-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-address',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormAddressComponent implements OnInit {
 
-  constructor() { }
+  public addressFormGroup: FormGroup;
+  public latitude: number;
+  public longitude: number;
+  public zoom = 15;
 
-  ngOnInit(): void {
+  constructor(
+    public formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.buildForm();
+    this.setCurrentLocation();
+  }
+
+  private setCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
+  }
+
+  markerDragEnd($event: any) {
+    this.addressFormGroup.patchValue({
+      latitud: $event.coords.lat,
+      longitud: $event.coords.lng
+    });
+  }
+
+  buildForm() {
+    this.addressFormGroup = this.formBuilder.group({
+      direccion: ['', Validators.required],
+      ciudad: ['', Validators.required],
+      localidad: ['', Validators.required],
+      codigoPostal: ['', Validators.required],
+      aclaraciones: [''],
+      latitud: ['', Validators.required],
+      longitud: ['', Validators.required]
+    });
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { FormAddressComponent } from './components/dialogs/form-address/form-add
 import { SelectAddressComponent } from './components/dialogs/select-address/select-address.component';
 import { BaseComponent } from './components/dialogs/base/base.component';
 import { DialogService } from './components/dialogs/dialog.service';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,11 @@ import { DialogService } from './components/dialogs/dialog.service';
     ReactiveFormsModule,
     FormsModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyARaDrtQwfk6Ql8byHHSJtAAJGswf9ueds',
+      libraries: ['places']
+    })
   ],
   exports: [
     HttpClientModule,
