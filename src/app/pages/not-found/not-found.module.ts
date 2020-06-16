5f0bc6e05b08155501bbbeba5a09150cdda67c74
Aaron Castro <aaroncrcr@gmail.com>
2020-06-16 20:00:16 -0300

    not-found page

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
     CommonModule,
-    NotFoundRoutingModule
+    NotFoundRoutingModule,
+    MatButtonModule
   ]
 })
 export class NotFoundModule { }
