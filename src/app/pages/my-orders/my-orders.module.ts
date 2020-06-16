46b3bc6e58527956841e788512e27e344b87a68a
Aaron Castro <aaroncrcr@gmail.com>
2020-06-16 10:00:46 -0300

    my-orders v1

--- src/app/pages/my-orders/my-orders.module.ts
+++ src/app/pages/my-orders/my-orders.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MyOrdersRoutingModule } from './my-orders-routing.module';
 import { MyOrdersComponent } from './my-orders.component';
+import { OrderCardComponent } from './order-card/order-card.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [MyOrdersComponent],
+  declarations: [MyOrdersComponent, OrderCardComponent],
   imports: [
     CommonModule,
-    MyOrdersRoutingModule
+    MyOrdersRoutingModule,
+    SharedModule
   ]
 })
 export class MyOrdersModule { }
