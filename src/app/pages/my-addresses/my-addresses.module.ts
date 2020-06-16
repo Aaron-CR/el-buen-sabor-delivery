1e8a72a0f065bfdda33cce085109229fda6bd151
Aaron Castro <aaroncrcr@gmail.com>
2020-06-15 23:41:52 -0300

    my-addresses v1

--- src/app/pages/my-addresses/my-addresses.module.ts
+++ src/app/pages/my-addresses/my-addresses.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MyAddressesRoutingModule } from './my-addresses-routing.module';
 import { MyAddressesComponent } from './my-addresses.component';
+import { AddressCardComponent } from './address-card/address-card.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [MyAddressesComponent],
+  declarations: [MyAddressesComponent, AddressCardComponent],
   imports: [
     CommonModule,
-    MyAddressesRoutingModule
+    MyAddressesRoutingModule,
+    SharedModule
   ]
 })
 export class MyAddressesModule { }
