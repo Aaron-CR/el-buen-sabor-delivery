6e8c6b296aeeb197f6a34dd1d3b05ab916cfe69b
Aaron Castro <aaroncrcr@gmail.com>
2020-06-18 23:13:11 -0300

    reformat assets folder

--- src/app/pages/menu/card/card.component.ts
+++ src/app/pages/menu/card/card.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  image = 'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80';
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/menu/dialog/dialog.component.ts
+++ src/app/pages/menu/dialog/dialog.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
+  image = 'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80';
   selected = '1';
 
   constructor() { }
