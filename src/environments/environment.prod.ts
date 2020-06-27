b6ba1575295462a16bcefe8f8050c3d85f82b403
Aaron Castro <aaroncrcr@gmail.com>
2020-06-27 12:20:24 -0300

    firebase hosting config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCcVf7VISioriT8I7gyGB9Tgxng6ETdQGo',
+    authDomain: 'elbuensabor-delivery.firebaseapp.com',
+    databaseURL: 'https://elbuensabor-delivery.firebaseio.com',
+    projectId: 'elbuensabor-delivery',
+    storageBucket: 'elbuensabor-delivery.appspot.com',
+    messagingSenderId: '126762949811',
+    appId: '1:126762949811:web:f8cbae9415e5dff80624c6',
+   measurementId: 'G-SPSBJX3N7P'
+  }
 };
